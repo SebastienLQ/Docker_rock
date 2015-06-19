f49428c2518553d0e3f76c044373fe00aa5bcfac
sebastien Le Querec <sebastien@lequerec.fr>
2015-06-19 10:12:18 +0200

    update views for images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,8 +1,10 @@
 class ImagesController < ApplicationController
   def index
     @images = Docker::Image.all
+
   end
   def show
     @image = Docker::Image.get(params[:id])
+    @history = @image.history
   end
 end
