814c15157d4e16263eece3b14fc3fe60c6f2f97b
sebastien Le Querec <sebastien@lequerec.fr>
2015-06-26 12:48:24 +0200

    modif mineure graphique des pages d'erreurs

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,13 @@
 class SearchController < ApplicationController
 skip_before_action :verify_authenticity_token
-  def create       
-    @containers = params[:format]
-    @search = Docker::Image.search('term' => params[:image])
+  def create 
+  begin      
+	    @containers = params[:format]
+	    @search = Docker::Image.search('term' => params[:image])
+  rescue
+  	redirect_to error_path(' internet is unreachable ')
     #Docker::Container.create('Image' => params[:image], 'Cmd' => params[:cmd])
+	end
   end
 
 end
